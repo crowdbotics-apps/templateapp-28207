ad808fd19c2c2d013058ae8a2dd4fcfbe4a65b26
Crowdbotics <team@crowdbotics.com>
2021-06-24 16:43:33 +0000

    Update models

--- home/models.py
+++ home/models.py
@@ -1,1 +1,17 @@
+from django.conf import settings
 from django.db import models
+
+
+class Login(models.Model):
+    "Generated Model"
+    user_id = models.IntegerField()
+    username = models.CharField(
+        max_length=256,
+        null=True,
+        blank=True,
+    )
+    password = models.CharField(
+        max_length=256,
+        null=True,
+        blank=True,
+    )
--- templateapp_28207/wsgi.py
+++ templateapp_28207/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'templateapp_28207.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "templateapp_28207.settings")
 
 application = get_wsgi_application()
